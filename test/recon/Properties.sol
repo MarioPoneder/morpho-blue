4b0b004e4ce734b3bc7c3b0b5404629c096b1f35
Mario Poneder <m.poneder@decentra.vision>
2024-02-04 21:44:24 +0100

    feat: Implemented simply property that "breaks" once fuzzer found way to borrow tokens

--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -5,4 +5,12 @@ pragma solidity ^0.8.0;
 import {Asserts} from "@chimera/Asserts.sol";
 import {Setup} from "./Setup.sol";
 
-abstract contract Properties is Setup, Asserts {}
+import {Market,IMorpho} from "src/interfaces/IMorpho.sol";
+
+abstract contract Properties is Setup, Asserts {
+    function crytic_somethingWasBorrowed() public view returns (bool) {
+        // this requires the fuzzer to supply collateral & loan tokens first
+        // and then successfully borrow
+        return IMorpho(address(morpho)).market(marketId).totalBorrowAssets == 0;
+    }
+}
