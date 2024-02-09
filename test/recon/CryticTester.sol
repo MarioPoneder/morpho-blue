de81eaa2fa14b89590cb9413c694e080ab20311a
Mario Poneder <m.poneder@decentra.vision>
2024-02-09 23:12:54 +0100

    feat: Added assertion test about interest accrual

--- test/recon/CryticTester.sol
+++ test/recon/CryticTester.sol
@@ -4,11 +4,36 @@ pragma solidity ^0.8.0;
 
 import {TargetFunctions} from "./TargetFunctions.sol";
 import {CryticAsserts} from "@chimera/CryticAsserts.sol";
+import {vm} from "@chimera/Hevm.sol";
+
+import {IMorpho} from "src/interfaces/IMorpho.sol";
 
 // echidna . --contract CryticTester --config echidna.yaml
 // medusa fuzz
 contract CryticTester is TargetFunctions, CryticAsserts {
     constructor() payable {
         setup();
+        
+        // prepare market 2 with collateral & supply
+        vm.prank(SENDER_1);
+        morpho.supplyCollateral(marketParams2, 10_000e18, SENDER_1, hex"");
+        vm.prank(SENDER_2);
+        morpho.supply(marketParams2, 10_000e18, 0, SENDER_2, hex"");
+    }
+
+    function assert_noInterestAccrued() public {
+        IMorpho _morpho = IMorpho(address(morpho));
+
+        // 'morpho_borrow2' has succeeded
+        precondition(_morpho.market(marketId2).totalBorrowAssets > 0);
+        // last market interaction / interest accrual was at least 100 seconds ago
+        precondition(_morpho.market(marketId2).lastUpdate < (block.timestamp - 100));
+
+        uint256 totalBorrowBeforeAccrued = _morpho.market(marketId2).totalBorrowAssets;
+        _morpho.accrueInterest(marketParams2);
+        uint256 totalBorrowAfterAccrued = _morpho.market(marketId2).totalBorrowAssets;
+
+        // assertion is triggered if nothing was borrowed
+        assert(totalBorrowAfterAccrued > totalBorrowBeforeAccrued);
     }
 }
--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -11,6 +11,8 @@ abstract contract Properties is Setup, Asserts {
     function crytic_somethingWasBorrowed() public view returns (bool) {
         // this requires the fuzzer to supply collateral & loan tokens first
         // and then successfully borrow
+
+        // returns false if something was borrowed
         return IMorpho(address(morpho)).market(marketId).totalBorrowAssets == 0;
     }
 }
